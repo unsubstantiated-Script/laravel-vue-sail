5d34116c9d9616ad625d4656284f70978fe58320
Justin Fulton <jusfulton@gmail.com>
2022-04-21 06:28:02 -0700

    Initials

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+//Added these in
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    //
+}
